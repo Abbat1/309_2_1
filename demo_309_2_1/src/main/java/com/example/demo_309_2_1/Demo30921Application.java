9f0dd651d4bf7b465dc604a4b674125e36e20af1
Abbat1 <cozzyyist@gmail.com>
2024-08-26 08:44:51 -0500

    demo309_2_1

---
+++ demo_309_2_1/src/main/java/com/example/demo_309_2_1/Demo30921Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_309_2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo30921Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo30921Application.class, args);
+	}
+
+}
---
+++ demo_309_2_1/src/test/java/com/example/demo_309_2_1/Demo30921ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_309_2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo30921ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
